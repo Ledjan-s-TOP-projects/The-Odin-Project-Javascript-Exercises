14ce24de71e5c49010223710de8d166205462e95
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-14 17:54:48 +0200

    16_findTheOldest completed

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function calculateAge(person) {
+  let death = person.yearOfDeath || new Date().getFullYear();
+  let age = death - person.yearOfBirth;
+  return age;
+}
 
+const findTheOldest = function (array) {
+  let oldest = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (calculateAge(array[i]) > calculateAge(oldest)) {
+      oldest = array[i];
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/solution/findTheOldest-solution.js
+++ 16_findTheOldest/solution/findTheOldest-solution.js
@@ -16,6 +16,26 @@ const findTheOldest = function (people) {
   });
 };
 
+const products = [
+  { name: "Laptop", price: 1200 },
+  { name: "Phone", price: 800 },
+  { name: "Tablet", price: 600 },
+  { name: "Monitor", price: 300 },
+];
+
+function getPrice(item) {
+  const price = item.price;
+  return price;
+}
+
+const mostExpensive = function (array) {
+  array.reduce((mostExpItem, currentItem) => {
+    const mostExpPrice = getPrice(mostExpItem);
+    const currentPrice = getPrice(currentItem);
+    return mostExpPrice < currentPrice ? currentItem : mostExpItem;
+  });
+};
+
 /* ALTERNATIVE SOLUTION
 const getAge = function (person) {
   // The nullish coalescing assignment operator
