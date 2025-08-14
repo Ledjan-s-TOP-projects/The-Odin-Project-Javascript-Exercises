bb1681223043a3e5f87064ded68aa3d71153a2e2
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-14 09:51:21 +0200

    14_fibonacci completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const number = Number(num);
 
+  //Handle edge cases
+  if (number < 0) return "OOPS";
+  if (number === 0) return 0;
+  if (number === 1 || number === 2) return 1;
+
+  //Initial first two Fibonacci numbers
+  let prevPrev = 1;
+  let prev = 1;
+  let current;
+
+  //Computer Fibonacci sequence iteratively
+  for (let i = 3; i <= number; i++) {
+    // current Fibonacci number = sum of previous two
+    current = prevPrev + prev;
+    prevPrev = prev;
+    prev = current;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
