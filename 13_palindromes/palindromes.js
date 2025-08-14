8e03df9297b7d8a54bad0adac7ca4826c5ecd821
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-14 09:13:46 +0200

    13-palindromes completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let cleanedString = string.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+  let reversedString = cleanedString.split("").reverse().join("");
+  return reversedString === cleanedString;
 };
 
 // Do not edit below this line
